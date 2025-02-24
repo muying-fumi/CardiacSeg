e56ec1983ab96181e2ab097dbd3185cb4830d784
muying-fumi <55749261+muying-fumi@users.noreply.github.com>
2025-02-24 23:17:20 +0800

    Add files via upload

--- setup_new.sh
+++ setup_new.sh
@@ -1,11 +1,8 @@
 sudo apt update && \
 sudo apt install ffmpeg libsm6 libxext6  -y && \
-#pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113 && \
-#pip install torch==2.0.0 torchvision==0.15.1 torchaudio==2.0.1 --extra-index-url https://download.pytorch.org/whl/cu113 && \
 python -m pip install --upgrade pip setuptools wheel
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
-pip install "monai[nibabel, tqdm, einops]==1.2.0" && \
-#pip install monailabel==0.7.0 && \
+pip install "monai[nibabel, tqdm, einops]" && \
 pip install monailabel && \
 pip install timm && \
 pip install tensorboard && \
